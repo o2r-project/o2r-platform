a492b92cb2a6089340af08ff5ec0baa2d59ca805
jansule <j_sule02@wwu.de>
2016-04-22 13:25:22 +0200

    add display of .jpg-files(unstable) - updated publications-structure - display of .pdf-files buggy

--- js/app.js
+++ js/app.js
@@ -24,9 +24,40 @@
     
 app.controller('AuthorCtrl', function($scope, publications, pdf_files, $stateParams){
 		$scope.publications = publications.getPublications();
+
+        /*
 		if ($stateParams.id != ""){
 			var currentPublication = pdf_files.showPDF(publications.getEntryById($stateParams.id).url);			
-		}
+        }
+    */
+        // -----------NEW---------
+        if($stateParams.id != ""){
+            
+            //
+            // handles display of files regarding filetype peculiarities
+            //
+            switch(publications.getContentById($stateParams.id).type){
+                case '.pdf':
+                    _setAllFalse;
+                    var currentContent = pdf_files.showPDF(publications.getContentById($stateParams.id).url);
+                    publications.getContentById($stateParams.id).show = true;
+                    break;
+                case '.jpg':
+                    _setAllFalse;
+                    var currentContent = publications.getContentById($stateParams.id).show = true;
+                    break;
+            }
+            
+
+        }
+
+        // sets .show-Attribute of all files to false
+        var _setAllFalse = function(){
+             for(file in publications.getPublications().content){
+                        publications.getPublications().content[file].show = false;
+                    }
+        }; 
+        
 
 });
 
--- services/publications.js
+++ services/publications.js
@@ -1,39 +1,72 @@
 app.factory("publications", function(){
-	var publications = [
-		{
+	var publications = {
 			id: 1,
 			title: 'Title 1',
 			author: 'Author 1',
-			year: "2016",
-			url: "https://statistics.stanford.edu/sites/default/files/EFS%20NSF%20474.pdf"
-		},
-		{
-			id: 2,
-			title: 'Title 2',
-			author: 'Author 2',
-			year: "2016",
-			url: "http://infoscience.epfl.ch/record/136640/files/VandewalleKV09.pdf"
-		},
-		{
-			id: 3,
-			title: 'Title 3',
-			author: 'Author 3',
-			year: "2016",
-			url: "http://ifgi.uni-muenster.de/~epebe_01/eos.pdf"
-		},
-		{
-			id: 4,
-			title: 'Title 4',
-			author: 'Author 4',
-			year: "2016",
-			url: "http://www.math.usu.edu/~corcoran/classes/14spring6550/handouts/reproducible_research.pdf"
-		},	
-	];
+			year: '2016',
+			content: [
+				{
+					id: 1,
+					type: '.pdf',
+					name: 'EFS%20NSF%20474.pdf',
+					url: 'https://statistics.stanford.edu/sites/default/files/EFS%20NSF%20474.pdf',
+					show: false
+				},
+				{
+					id: 2,
+					type: '.jpg',
+					name: 'Alte_Herrenh%C3%A4user_Stra%C3%9Fe_10_Hannover_Hardenbergsches_Haus_S%C3%BCdfront.jpg',
+					url: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Alte_Herrenh%C3%A4user_Stra%C3%9Fe_10_Hannover_Hardenbergsches_Haus_S%C3%BCdfront.jpg',
+					show: false
+				},
+				{
+					id: 3,
+					type: '.pdf',
+					name: 'VandewalleKV09.pdf',
+					url: 'http://infoscience.epfl.ch/record/136640/files/VandewalleKV09.pdf',
+					show: false
+				},
+				{
+					id: 4,
+					type: '.pdf',
+					name: 'eos.pdf',
+					url: 'http://ifgi.uni-muenster.de/~epebe_01/eos.pdf',
+					show: false
+				},
+				{
+					id: 5,
+					type: '.pdf',
+					name: 'reproducible_research.pdf', 
+					url: 'http://www.math.usu.edu/~corcoran/classes/14spring6550/handouts/reproducible_research.pdf',
+					show: false
+				},
+				{
+					id: 6,
+					type: '.jpg',
+					name: 'DMI_conference.jpg',
+					url:'https://upload.wikimedia.org/wikipedia/commons/d/dc/DMI_conference.jpg',
+					show: false
+				}
+			]	
+	};
 	
 	var get = function(){
 		return publications;
 	};
 	
+	var getAllContent = function(){
+		return publications.content;
+	};
+
+	var findContent = function(searchedId){
+		for(content in publications.content){
+			if(publications.content[content].id == searchedId){
+				return publications.content[content];
+			}
+		}
+		return null;
+	};
+
 	var findEntry = function(searchedID){
 		for (publication in publications){
 			if (publications[publication].id == searchedID){
@@ -45,6 +78,8 @@ app.factory("publications", function(){
 	
 	return {
 		getPublications: get,
-		getEntryById: findEntry
+		getEntryById: findEntry,
+		getAllContent: getAllContent,
+		getContentById: findContent
 	}; 		
 });
