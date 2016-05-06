ff57781cdb437fb69fccfd883ad6e3042afad585
jansule <j_sule02@wwu.de>
2016-05-06 13:31:27 +0200

    fixed display of pdf

--- directives/o2rPdf.js
+++
@@ -1,9 +0,0 @@
-app.directive('o2rPdf', function() {
-    return {
-        restrict: 'E',
-        link: function(scope, element, attrs) {
-            var url = scope.$eval(attrs.ngSrc);
-            element.replaceWith('<object type="application/pdf" data="' + url + '"></object>');
-        }
-    };
-});
