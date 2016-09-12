3f2d592d80f39432cccb23d7c169db817870041d
jansule <j_sule02@uni-muenster.de>
2016-09-12 17:49:09 +0200

    move image files and update configSample.js

--- client/app/config/configSample.js
+++ client/app/config/configSample.js
@@ -1,11 +1,9 @@
-(function(){
-    'use strict';
+'use strict';
 
-    angular
-        .module('starter')
-        .constant('url', /*put url as string here*/)
-        .constant('sizeRestriction', 10000000)
-        .constant('disableTracking', false);
-        const piwik = {};
-        piwik.url = "//o2r.uni-muenster.de/piwik/";
-})();
+angular
+    .module('starter')
+    .constant('url', /*put url as string here*/)
+    .constant('sizeRestriction', 10000000)
+    .constant('disableTracking', false);
+const piwik = {};
+piwik.url = "//o2r.uni-muenster.de/piwik/";
