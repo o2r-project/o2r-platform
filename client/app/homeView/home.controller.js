faed27c5b891d8f482b3861e11faf7807f3f7b5c
MarkusKonk <mkonkol@arcor.de>
2017-03-16 14:28:48 +0100

    Put url parameters in form automatically

--- client/app/homeView/home.controller.js
+++ client/app/homeView/home.controller.js
@@ -19,6 +19,18 @@
         
         ///////////
 
+        if(window.location.href.indexOf('shareURL') >= 0){
+            parseURL(window.location.href);
+        }
+        
+        ///////////
+        //Working example: http://localhost/#!/home?shareURL=https://uni-muenster.sciebo.de/index.php/s/m7k16mNmfDbSO0P&path=/metatainer
+        function parseURL(url){
+            url = url.split('%2F').join('/');
+            vm.scieboUrl = url.split('shareURL=')[1].split('&')[0];
+            vm.scieboPath = url.split('path=/')[1];
+        }
+
         function sendScieboUrl(){
             var progressbar = ngProgressFactory.createInstance();
 			progressbar.setHeight('3px');
