c4da93e0286506e3918e37fbf6ece0948f51e0bb
markuskonk <mkonkol@arcor.de>
2017-10-24 10:11:55 +0200

    highlight candidates and link to edit form #100

--- client/app/authorView/author.controller.js
+++ client/app/authorView/author.controller.js
@@ -25,6 +25,9 @@
             vm.showAdmin = () => ((getUserGroup(author.data.level) == 'Admins') || (getUserGroup(author.data.level) == 'Editors'));
 
             logger.info('vm.allPubs: ',vm.allPubs);
+            for (var i=0; i< vm.allPubs.length;i++){
+                console.log(vm.allPubs[i].candidate)
+            }
 
             $scope.$on('$destroy', function(){
                 $mdDialog.cancel();
