aec52d1f7039a04506256f0bc3f83e67303f4675
Torben <t.kraf03@gmail.com>
2017-11-03 18:14:08 +0100

    finish candidateView, dropdown and add substitution POST

--- client/app/substituteView/substitute.controller.js
+++ client/app/substituteView/substitute.controller.js
@@ -15,108 +15,115 @@
         vm.icons = icons;
         vm.similarPubs = getMetadataSimComp();
         vm.publication = erc;
-        // $scope.filePairs = []; // = filePairsForSubstitution;
-        // $scope.ngRepeatCounter = 0;
-
-        vm.substituteOptions = getSubstituteOptions;
-
+        $scope.substituteOptions = getSubstituteOptions;
         $scope.baseType = "base";
         $scope.overlayType = "overlay";
-
         $scope.substitutionRows = [{}];
         $scope.baseCandidates = [];
         $scope.overlayCandidates = [];
         $scope.selectedSubstitutionFiles = [];
-
         $scope.delDropdown = delDropdown;
         $scope.addDropdown = addDropdown;
 
-
+        // set width of div with ERC title in selection list
         var substERCtitleWidth = $("#substitutionErcSelectionViewTitle").context.body.clientWidth;
-        vm.divWidth = parseInt((substERCtitleWidth/100 + 5), 10);
+        vm.divWidth = parseInt((substERCtitleWidth/100 + 10), 10);
 
         //////////////////
 
-        $scope.$on('loadedSimilarComps', function(event, data){ //similarPubs will be set to comp_meta from metadata factory
+        $scope.$on('loadedSimilarComps', function(event, data){
+            // similarPubs will be set to comp_meta from metadata factory
             vm.similarPubs = data;
         });
 
-        // $scope.toggleSubstitutionFile = function(file, selected, ngCounter) {
-        //   if (file && selected == undefined) {console.log("undefined ---");}
-        //
-        //   if (file && selected != undefined) {
-        //     console.log("toggleSubstitutionFile");
-        //     console.log(file);
-        //     console.log(selected);
-        //     console.log(ngCounter);
-        //
-        //       var idx = selected.indexOf(file);
-        //       if (idx > -1) {
-        //           selected.splice(idx, 1);
-        //       }
-        //       else {
-        //           selected.push(file);
-        //       }
-        //   }
-        // };
-        //
-        // $scope.exists = function(file, selected) {
-        //     if (file && selected == undefined) {console.log("undefined ---");}
-        //
-        //     if (file && selected != undefined) {
-        //         return selected.indexOf(file) > -1;
-        //     }
-        // };
-
         $scope.cancel = function() {
             $scope.substitutionRows = [{}];
-            // $scope.baseCandidates = [];
-            // $scope.overlayCandidates = [];
-            // $scope.selectedSubstitutionFiles = [];
-
-            $mdDialog.hide();
-            // $mdDialog.cancel();
+            $mdDialog.cancel();
         };
 
         // TODO: start substitution UI
         $scope.startSubstitutionUI = function() {
-            console.log("initiateSubstitution");
             logger.info("initiateSubstitution");
-            window.alert("substitute clicked!");
-
-            console.log("$scope.substitutionRows");
-            console.log($scope.substitutionRows);
+            logger.debug("initiateSubstitution");
 
             if (!Array.isArray($scope.substitutionRows) || $scope.substitutionRows == undefined || $scope.substitutionRows.length == 0 || !checkForFiles($scope.substitutionRows)) {
+                logger.info("no substitution files choosen");
                 window.alert("please choose files");
             } else {
+                $mdDialog.hide();
+                logger.info("init substitution");
+                // start substitution
 
+                let substitutionMetadata = {};
+                let arrayOfSubstitutionObjects = [];
+
+
+                console.log($scope.substitutionRows);
+                console.log(vm.base);
+                console.log(vm.overlay);
+
+                substitutionMetadata.base = vm.base.id;
+                substitutionMetadata.overlay = vm.overlay.id;
+                substitutionMetadata.substitutionFiles = [];
+
+                for (let i=0; i<$scope.substitutionRows.length; i++) {
+                  let object_ = {};
+                  object_.base = $scope.substitutionRows[i].basefile.filePath;
+                  object_.overlay = $scope.substitutionRows[i].overlayfile.filePath;
+                  substitutionMetadata.substitutionFiles.push(object_);
+                }
+
+                console.log(substitutionMetadata);
+
+                httpRequests.substitute(substitutionMetadata)
+                  .then(function(res){
+                      console.log(res);
+                  })
+                  .catch(function(err){
+                    console.log(err);
+                  });
             }
-
-            // var selectedFiles = $scope.selectedSubstitutionFiles;
-            // if (selectedFiles != undefined || selectedFiles.length == 0) {
-            //     logger.info("init substitution");
-            // } else {
-            //     logger.info("no substitution files choosen");
-            // }
         };
 
         // on change of dropdown list
         $scope.selectSubstitutionFiles = function(file, type, row) {
-            // console.log(file);
-            // console.log(file.name);
-            // console.log(type);
-            // console.log(row);
-            // console.log($scope.substitutionRows);
-
             let i = row.$index;
+            let baseCandidatesLength = $scope.baseCandidates.length;
+            let overlayCandidatesLength = $scope.overlayCandidates.length;
+            let oldBaseCandidatePosition; // = $scope.substitutionRows[i].basefile.$$mdSelectId;
+            let newBaseCandidatePosition;
+            let oldOverlayCandidatePosition; // = $scope.substitutionRows[i].overlayfile.$$mdSelectId;
+            let newOverlayCandidatePosition;
+
+            // change "selected" to TRUE of new file
             if (type == "base") {
+                newBaseCandidatePosition = file.$$mdSelectId;
+                // $$mdSelectId will add up all files that have been inside of md-select (when cancelling md-dialog and open again)
+                while (newBaseCandidatePosition > (baseCandidatesLength + overlayCandidatesLength)) {
+                    newBaseCandidatePosition = newBaseCandidatePosition - (baseCandidatesLength + overlayCandidatesLength);
+                }
+                // change "selected" to FALSE of exchanged file
+                if ($scope.substitutionRows[i].basefile != undefined) {
+                    oldBaseCandidatePosition = $scope.substitutionRows[i].basefile.$$mdSelectId;
+                    $scope.baseCandidates[oldBaseCandidatePosition-1].selected = false;
+                }
                 $scope.substitutionRows[i].basefile = file;
+                $scope.baseCandidates[newBaseCandidatePosition-1].selected = true;
             }
             if (type == "overlay") {
+                newOverlayCandidatePosition = file.$$mdSelectId;
+                // $$mdSelectId will add up all files that have been inside of md-select (when cancelling md-dialog and open again)
+                while (newOverlayCandidatePosition > (baseCandidatesLength + overlayCandidatesLength)) {
+                    newOverlayCandidatePosition = newOverlayCandidatePosition - (baseCandidatesLength + overlayCandidatesLength);
+                }
+                // change "selected" to FALSE of exchanged file
+                if ($scope.substitutionRows[i].overlayfile != undefined) {
+                    oldOverlayCandidatePosition = $scope.substitutionRows[i].overlayfile.$$mdSelectId;
+                    $scope.overlayCandidates[oldOverlayCandidatePosition-1-baseCandidatesLength].selected = false;
+                }
                 $scope.substitutionRows[i].overlayfile = file;
+                $scope.overlayCandidates[newOverlayCandidatePosition-1-baseCandidatesLength].selected = true;
             }
-            console.log($scope.substitutionRows);
         };
 
         //////////////////
@@ -134,9 +141,7 @@
         };
 
         function checkForFiles(array) {
-          // true = contains files
-          // false = no files
-
+          // true = contains files , false = no files
             if (Array.isArray(array)) {
                 for (let i=0; i<array.length; i++) {
                     if (typeof(array[i].basefile) != "object" || typeof(array[i].overlayfile) != "object") {
@@ -151,87 +156,76 @@
         // function to provide files for substitution as candidates
         function getSubstituteOptions(event, pub) {
             logger.info("base and overlay ERC choosen for substitution");
-            //vm.selectedSubstitutionFiles = [];  // TODO: maybe not necessary to be always empty -- how about examples for user?
             vm.base = vm.publication;
             vm.overlay = pub;
-            // console.log(vm);
-            $scope.filePairs = [];
-            $scope.baseCandidates = [];
-            $scope.overlayCandidates = [];
 
-            // TODO: put filenames relevant for substitution in array
-            vm.basefiles = getFileCandidates(vm.base);
-            vm.overlayfiles = getFileCandidates(vm.overlay);
+            if (vm.base.metadata.o2r.inputfiles.length < 1 || vm.overlay.metadata.o2r.inputfiles.length < 1) {
+                logger.info("no inputfiles for substitution");
+                logger.debug("no inputfiles for substitution");
+                window.alert("no substitution possible");
+            } else {
+                $scope.filePairs = [];
+                $scope.baseCandidates = [];
+                $scope.overlayCandidates = [];
 
-            $scope.baseCandidates = getFileCandidates(vm.base);
-            $scope.overlayCandidates = getFileCandidates(vm.overlay);
+                // put filenames relevant for substitution in array
+                vm.basefiles = getFileCandidates(vm.base);
+                vm.overlayfiles = getFileCandidates(vm.overlay);
 
-            console.log("baseCandidates");
-            console.log($scope.baseCandidates);
-            console.log("overlayCandidates");
-            console.log($scope.overlayCandidates);
+                $scope.baseCandidates = getFileCandidates(vm.base);
+                $scope.overlayCandidates = getFileCandidates(vm.overlay);
 
-            // var files01 = ["text.csv", "main.Rmd", "data.txt", ["hallo.txt", "jojo"]];
-            // var files01 = ["text.csv", "main.Rmd", "data.txt", "jojo"];
-            // var files02 = ["text.txt", "main.Rmd", "timeseries.csv", "Dockerfile"];
-            //
-            // var thisArray = [];
-            //
-            // var candidate01 = new substitutionCandidate(files01[0], files02[2]);
-            // var candidate02 = new substitutionCandidate(files01[2], files02[0]);
-            // var candidate03 = new substitutionCandidate(files01[1], files02[3]);
-            // $scope.filePairs.push(candidate01);
-            // // $scope.filePairs.push(candidate02);
-            // // $scope.filePairs.push(candidate03);
-            // console.log($scope.filePairs);
-
-            // vm.basefiles = getFileCandidates(files01);
-            // vm.overlayfiles = getFileCandidates(files02);
-
-            $mdDialog.show({
-                contentElement: '#showCandidateView',
-                parent: angular.element(document.body),
-                scope: $scope,
-                multiple: false,
-                targetEvent: event,
-                clickOutsideToClose: false
-            });
+                $mdDialog.show({
+                    contentElement: '#showCandidateView',
+                    parent: angular.element(document.body),
+                    scope: $scope,
+                    preserveScope: true,
+                    multiple: false,
+                    targetEvent: event,
+                    clickOutsideToClose: false
+                });
+            }
         };
 
-        // function filePairsForSubstitution() {
-        //   console.log("filePairsForSubstitution");
-        // }
-
-        //TODO: to implement
         function getFileCandidates(pub) {
             var candidatesArray = [];
-            var filesArray;
-            var thisArray = pub.files.children;
-            for (let i=0; i<thisArray.length; i++) {
-                if (pub.files.children[i].children && Array.isArray(pub.files.children[i].children)) {
-                    filesArray = pub.files.children[i];
-                    i = thisArray.length-1;
-                }
-            };
-            filesArray = filesArray.children;
-            for (let j=0; j<filesArray.length; j++) {
-                if (Array.isArray(filesArray[j].children) || (filesArray[j].extension == undefined) || (filesArray[j].extension == null)) {
+            var inputFilesArray = pub.metadata.o2r.inputfiles;
+
+            for (let i=0; i<inputFilesArray.length; i++) {
+                let candidateObject = {};
+                let filePath = inputFilesArray[i];
+                let char_ = filePath.indexOf("/");
+                filePath = filePath.substring(char_+1); // only filename with extension and datapath (without ERC_ID)
+
+                candidateObject.filePath = filePath;
+                candidateObject.selected = false;
+                // in case the inputFile is in folders in the ERC_ID-folder
+                if (filePath.lastIndexOf("/") < 0) {
+                    candidateObject.fileName = filePath;
                 } else {
-                  candidatesArray.push(filesArray[j]);
+                    let char_2 = filePath.lastIndexOf("/");
+                    candidateObject.fileName = filePath.substring(char_2+1);
                 }
-            };
+                candidatesArray.push(candidateObject);
+            }
             return candidatesArray;
         };
 
-
-
         function delDropdown(row) {
             var i = row.$index;
-            $scope.substitutionRows.splice(i, 1);
+            if ($scope.substitutionRows.length <= 1) {
+                // TODO: button in red to warn
+            } else {
+              $scope.substitutionRows.splice(i, 1);
+            }
         };
 
         function addDropdown() {
-            $scope.substitutionRows.push({});
+          if ($scope.substitutionRows.length <= $scope.baseCandidates.length && $scope.substitutionRows.length <= $scope.overlayCandidates) {
+              $scope.substitutionRows.push({});
+          } else {
+              // TODO: button in red to warn
+          }
         };
 
     }
